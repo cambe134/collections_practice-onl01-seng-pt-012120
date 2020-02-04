62f64599ac40fa1e2b8ca3229fbe5d6cbeb12158
Spencer Cambern <cambern134@gmail.com>
2020-02-04 01:01:22 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,7 +2,7 @@ def sort_array_asc(array)
   array.sort
 end
 
-sort_array_asc([25, 7, 1]) 
+sort_array_asc([25, 7, 1])
 
 def sort_array_desc(array)
   array.sort do | left, right|
@@ -10,7 +10,7 @@ def sort_array_desc(array)
   end
 end
 
-sort_array_desc([25, 7, 14]) 
+sort_array_desc([25, 7, 14])
 
 def sort_array_char_count(array)
   array.sort do |left, right|
@@ -18,20 +18,20 @@ def sort_array_char_count(array)
   end
 end
 
-sort_array_char_count(["dogs", "cat", "Horses"]) 
+sort_array_char_count(["dogs", "cat", "Horses"])
 
 def swap_elements(array)
   array[1], array[2] = array[2], array[1]
   array
 end
 
-swap_elements(["blake", "ashley", "scott"]) 
+swap_elements(["blake", "ashley", "scott"])
 
 def reverse_array(array)
   array.reverse
 end
 
-reverse_array(["blake", "ashley", "scott"]) 
+reverse_array(["blake", "ashley", "scott"])
 
 def kesha_maker(array)
   array.each do |item|
@@ -39,17 +39,17 @@ def kesha_maker(array)
   end
 end
 
-kesha_maker(["blake", "ashley", "scott"]) 
+kesha_maker(["blake", "ashley", "scott"])
 
 def find_a(array)
   array.find_all do |word|
     word[0] == "a"
   end
 
-  
+
 end
 
-find_a(["apple", "orange", "pear", "avis", "arlo", "ascot" ]) 
+find_a(["apple", "orange", "pear", "avis", "arlo", "ascot" ])
 
 def sum_array(array)
   sum = 0
@@ -57,7 +57,7 @@ def sum_array(array)
     sum+=num
   end
   sum
-end 
+end
 
 end
 
@@ -73,4 +73,4 @@ def add_s(array)
   end
 end
 
-add_s(["hand","feet", "knee", "table"]) 
+add_s(["hand","feet", "knee", "table"])
