ffd1e9dba10c7c1087a0d5fd0769de0ee620219f
Spencer Cambern <cambern134@gmail.com>
2020-02-04 01:17:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -8,7 +8,13 @@ def sort_array_desc (array)
   array.sort {|x,y| y <=> x }
 end
 
+def sort_array_char_count(array)
+  array.sort do |left, right|
+    left.length <=> right.length
+  end
+end
 
+sort_array_char_count(["dogs", "cat", "Horses"])
 
 def swap_elements (array)
   array[1], array[2] = array[2], array[1]
